a6216edd64d7daacd70adaf22b8fc0980f535133
100035796-lang <100035796@mvla.net>
2025-11-13 17:07:47 -0800

    Add files via upload

--- YahtzeeGame.java
+++ YahtzeeGame.java
@@ -5,6 +5,8 @@ public class YahtzeeGame {
 	private YahtzeeDice dice;
 	private int[] rollSpecificDie;
 	private boolean[] chosen = new boolean[]{false, false, false, false, false, false, false, false, false, false, false, false, false};
+	private int[] totalScores = new int[13];
+	private int roundNumber = 1;
 	Scanner sc;
 	
 	public YahtzeeGame() {
@@ -14,11 +16,26 @@ public class YahtzeeGame {
 	public void startGame() {
 		sc = new Scanner(System.in);
 		System.out.println("Welcome to Yahtzee with Bill");
-//		int scores = 0;
-		int dicenumber = 1;
-
 		for (int i = 0; i < 13; i++) {
-			int rolls = 0;
+			
+			playRound();
+		}
+		
+		System.out.println("The game is now finished!");
+		System.out.println("Here is your final scoreboard.");
+		displayScoreboard();
+		
+		int total = Arrays.stream(totalScores).sum();
+		System.out.println("Final Total Score: " + total);
+		System.out.println("Thanks for playing!");
+	}
+		
+
+		
+			
+	
+	public void playRound() {
+		int rolls = 0;
 			final int MAX_ROLLS = 3;
 			boolean keepRolling = true;
 			
@@ -27,7 +44,7 @@ public class YahtzeeGame {
 			while (keepRolling && rolls < MAX_ROLLS)	 {
 				rolls++;
 				int total = dice.roll(rollSpecificDie);
-				System.out.println("\nDice #" + dicenumber + ":");
+				System.out.println("\nRound #" + roundNumber + ":");
 				System.out.println("Roll #" + rolls + ":");
 				System.out.println(dice.toString());
 				System.out.println("Now displaying scores for Ones, Twos, Threes, Fours, Fives,");
@@ -44,7 +61,6 @@ public class YahtzeeGame {
 					
 					if (!response.equals("y")) {
 						keepRolling = false;
-						dicenumber++;
 				} else {
 					for (int u = 1; u < 6; u++) {
 						System.out.println("Do you want to save dice number "+u+"? (y/n)");
@@ -57,21 +73,26 @@ public class YahtzeeGame {
 				}
 				}
 					} else {
-						System.out.println("No rolls left. Thanks for playing!");
+						System.out.println("No rolls left.");
 				}
 			}
 			System.out.println("\nFinal dice/score:");
 			System.out.println(dice.toString());
+			System.out.println("[1s 2s 3s 4s 5s 6s 3k 4k FH SS LS Ch Yz]");
+			System.out.println(Arrays.toString(DiceScoreCalculator.getScored(dice.getDice())));
+			int[] scores = DiceScoreCalculator.getScored(dice.getDice());
+			displayScoreboard();
+			
+			int chosenScore = playerChooseScore(scores);
+			roundNumber++;
+			System.out.println("You scored " + chosenScore + " points this round");
 			System.out.println("Round Over. Bill wil now attempt to beat you up!");
 			System.out.println("Whoops! Bill died!");
 			billDisaster();
-			int[] scores = DiceScoreCalculator.getScored(dice.getDice());
+			System.out.println("Don't worry though! He's still willing to play with you!");
+			System.out.println("✧ദ്ദി( ˶^ᗜ^˶ )");
 			System.out.println(Arrays.toString(scores));
-		}
-		System.out.println("Game over");
-		System.out.println();
-	}
-		
+	}	
 	
 	public Die[] getDice() {
 		return dice.getDice();
@@ -82,27 +103,64 @@ public class YahtzeeGame {
 		do {
 			System.out.println("Please enter which score you want to claim between 1 and 13");
 			response = sc.nextInt() - 1;
+			sc.nextLine();
 		} while (!(response>=0 && response<13 && !chosen[response]));
 		
+		chosen[response] = true;
+		totalScores[response] = scores[response];
 		return scores[response];
-		
 	}
+
+    public void displayScoreboard() {
+        System.out.println("\nCurrent Scoreboard:");
+        System.out.println("[1s 2s 3s 4s 5s 6s 3k 4k FH SS LS Ch Yz]");
+        System.out.println(Arrays.toString(totalScores));
+    }
+
+    public void displayChosen() {
+        System.out.print("Chosen categories: ");
+        for (int i = 0; i < chosen.length; i++) {
+            if (chosen[i]) System.out.print((i + 1) + " ");
+        }
+        System.out.println();
+    }
 	
 	public void billDisaster() {
 		int sad = (int)(Math.random()*10+1);
 		switch (sad) {
 		case 1:
 			System.out.println("Bill got involved in a car crash on the Rodoviao dos Imigrantes Highway!");
+			break;
 		case 2:
-			System.out.println("Bill beat Kabir eight times in a row and Kabir killed him :(");
+			System.out.println("Bill beat Kabir eight times in a row and Kabir killed him! :(");
+			break;
 		case 3:
-			System.out.println("Bill tried to light a big balloon on fire. It didn't work out");
+			System.out.println("Bill tried to light a big balloon on fire. It didn't work out!");
+			break;
 		case 4: 
-			System.out.println("Bill thought he was Walter White and attemted to drink hydrochloric acid");
+			System.out.println("Bill thought he was Walter White and attemted to drink hydrochloric acid!");
+			break;
 		case 5:
-			System.out.
-		default: 
-			System.out.println("Kabir killed Bill!");
+			System.out.println("Bill accidentally slipped on a banana and fell to his death!");
+			break;
+		case 6:
+			System.out.println("Bill had severe allergies to pollen and accidentally sniffed a dandelion!");
+			break;
+		case 7:
+			System.out.println("Bill told a terrible joke at a bar and the collective light reflected");
+			System.out.println("from the eyerolls fried him!");
+			break;
+		case 8:
+			System.out.println("Bill won a single gamble at a casino and bragged so much his ego inflated");
+			System.out.println("and he floated away!");
+			break;
+		case 9:
+			System.out.println("Bill microwaved a spoon with his cereal and ended up killing himself!");
+			break;
+		case 10:
+			System.out.println("Bill claimed that he was \"too cool\" to hang out with his friends and");
+			System.out.println("spontaneously turned into an ice statue!");
+			break;
 		}
 	}
 }
